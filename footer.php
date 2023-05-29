65dae1339a4f549341542679e5251a01dfbae49c
Gaël Hébert-Furoy <118003096+Vaxs02@users.noreply.github.com>
2023-05-29 06:06:01 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<div class="container, p-3 mb-2 bg-white text white">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="col-md-4 d-flex align-items-center">
+        <span class="mb-3 mb-md-0 text-body-secondary">&copy; 2023 Vaxy Dev | Copyright, All rights reserved</span>
+    </div>
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li class="ms-3"><a class="text-body-secondary" href="https://youtube.com/@VaxyDev"><i class="bi bi-youtube"></i> </a></li>
+            <li class="ms-3"><a class="text-body-secondary" href="https://twitter.com/gael_hf01"><i class="bi bi-twitter"></i></a></li>
+            <li class="ms-3"><a class="text-body-secondary" href="https://instagram.com/gael___hf"><i class="bi bi-instagram"></i></a></li>
+        </ul>
+    </footer>
+</div>
