733dfe2d4ed1f28c43ed40bd81971b364ff36130
Gaël Hébert-Furoy <118003096+Vaxs02@users.noreply.github.com>
2023-06-09 07:32:48 -0400

    exemple.php

---
+++ exemple.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Vaxy Dev Blog</title>
+        <!-- Import BootStrap API -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+        <link rel="icon" type="image/x-icon" href="../assets/Logo.png">
+      </head>
+    <body>
+    <?php include '../navbar_articles.php'; ?>
+          <!-- Contenu -->
+          <div class="text-center">
+            <img src="./assets/terre.jpg" class="img-fluid">
+        </div>
+          <br>
+          <h1 style="color: black; font-size: 5rem; text-align: center;">Terre / Exemple</h1>
+          <br><br>
+          <p style="color: darkgrey; font-size: 1.5rem; text-align: center;">La planète Terre, notre foyer cosmique, est un joyau d'une beauté extraordinaire. Avec ses vastes océans, ses majestueuses montagnes et ses étendues verdoyantes, elle offre une diversité naturelle à couper le souffle. C'est sur cette sphère bleue que la vie a pris naissance, créant un écosystème complexe et interconnecté. La Terre est le seul endroit connu à ce jour où la vie prospère, abritant une myriade d'espèces fascinantes, des minuscules organismes microscopiques aux imposants mammifères. Cependant, notre belle planète est confrontée à des défis environnementaux majeurs, tels que le changement climatique et la perte de biodiversité. Il est de notre responsabilité de protéger et de préserver ce précieux héritage pour les générations futures.</p>
+          <br>
+          <p style="color: black; font-size: 1rem; text-align: left; margin-left: 0.5rem;"></strong>- Gaël Hébert-Furoy</strong></p>
+          <!-- Contenu -->
+          <!-- Footer -->
+          <div class="container, p-3 mb-2 bg-white text white">
+            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+              <div class="col-md-4 d-flex align-items-center">
+                <span class="mb-3 mb-md-0 text-body-secondary">&copy; 2023 Vaxy Dev | Copyright, All rights reserved</span>
+              </div>
+              <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                <li class="ms-3"><a class="text-body-secondary" href="https://youtube.com/@VaxyDev"><i class="bi bi-youtube"></i> </a></li>
+                <li class="ms-3"><a class="text-body-secondary" href="https://twitter.com/gael_hf01"><i class="bi bi-twitter"></i></a></li>
+                <li class="ms-3"><a class="text-body-secondary" href="https://instagram.com/gael___hf"><i class="bi bi-instagram"></i></a></li>
+              </ul>
+            </footer>
+          </div>
+        </body>
+</html>
